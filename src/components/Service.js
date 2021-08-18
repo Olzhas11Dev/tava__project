6e9ae322d9290207ebaef2217e75612151a4b2d0
Olzhas11Dev <olzhasdev11@gmail.com>
2021-08-19 04:13:18 +1200

    service section

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,27 +1,72 @@
-import React from 'react'
+import React,{ useRef,useState} from 'react'
 import '../style/service.css'
 import serviceDb from'../serviceDb'
+import { BsArrowLeft,BsArrowRight } from 'react-icons/bs' 
+import { AiOutlineMedicineBox,AiOutlineCloseCircle } from 'react-icons/ai' 
+
 
 
 function Service() {
 
+const slider = useRef(null)
+const[modal,setModal] = useState(false)
+const[chosenObject,setChosenObject] = useState('')
+
+ function handleToLeft (e){
+     e.preventDefault()
+    //  console.log(slider.current.offsetWidth)
+     slider.current.scrollLeft -= slider.current.offsetWidth/4
+    //  console.log(slider.current.scrollLeft)
+ }
+
+ function handleToRight (e){
+    e.preventDefault()
+    // console.log(slider.current.scrollLeft)
+    slider.current.scrollLeft += slider.current.offsetWidth/4
+}
+
+function showDetails(element){
+
+    setModal(true)
+   serviceDb.map((item)=>{
+      if(element.id===item.id){
+        setChosenObject(item)
+      }
+     return item
+   })
+}
+console.log(chosenObject,chosenObject.description)
     return (
         <div className='service_container' >
            <div className='service_transparentTitle'>Our Services</div>
             <h1>What we <span>offer</span></h1>
-            <div className='service_container_slice' >
-                <button>left</button>
-                <div className='service_row'>
+            <div className='service_container_slider' >
+                <BsArrowLeft className='service_btn_slider' onClick={handleToLeft}/>
+                <div className='service_row' ref={slider}>
                     {serviceDb.map((item)=>{
                         return (
-                            <div className='service_card' >
-                            <div key={item.id} className='service_item'>{item.title}</div>
+                            <div onClick={()=>showDetails(item)} key={item.id} className='service_card' >
+                                <AiOutlineMedicineBox className='service_icon' />
+                                <div className='service_title'>{item.title}</div>
+                                <div className='service_service'>{item.service}</div>
+                                
                             </div>
                         )
                     })} 
+                    
                 </div>
-                <button>right</button>
+                <BsArrowRight onClick={handleToRight} className='service_btn_slider'/>
            </div>
+           {modal ? <div className='service_modal'>
+               <div className='service_modal_conatiner' >
+                <h2 className='modal_title' >{chosenObject.title}</h2>
+                <p className='modal_description' >{chosenObject.description}</p>
+                <AiOutlineCloseCircle className='modal_close_button' onClick={()=>setModal(false)}/>
+                
+            </div>
+            </div>:null}
+            
+             
         </div>
     )
 }
--- src/serviceDb.js
+++ src/serviceDb.js
@@ -3,13 +3,13 @@ const serviceData = [
         id:0,
         title:'Pediatric Ortho', 
         service: 'pediatric optometric',
-        description : 'Our eye care center offers a full range of eye health services to clients of all ages, from children to older adults'
+        description : "If you're ready for an alternative to glasses and/or contacts look to us for co-management of LASIK, cataract, and other ocular surgery."
     },
     {
         id:1,
         title:'Cataract treatement', 
         service: 'Cataract diagnosis and treatement',
-        description : 'Our eye care center offers a full range of eye health services to clients of all ages, from children to older adults'
+        description : "The optometrists our practice provide eyewear prescriptions and offer corrective laser eye surgery co-management as well."
     },
     {
         id:2,
@@ -21,7 +21,7 @@ const serviceData = [
         id:3,
         title:'Pediatric Ortho', 
         service: 'pediatric optometric',
-        description : 'Our eye care center offers a full range of eye health services to clients of all ages, from children to older adults'
+        description : "If you're ready for an alternative to glasses and/or contacts look to us for co-management of LASIK, cataract, and other ocular surgery."
     },
     {
         id:4,
@@ -31,6 +31,42 @@ const serviceData = [
     },
     {
         id:5,
+        title:'Laser Eye surgery', 
+        service: 'Laser Eye and Lens surgery',
+        description : "We diagnose and manage ocular diseases such as Glaucoma, Macular Degeneration, Diabetic Retinopathy and Cataracts."
+    },
+    {
+        id:6,
+        title:'Pediatric Ortho', 
+        service: 'pediatric optometric',
+        description : "If you're ready for an alternative to glasses and/or contacts look to us for co-management of LASIK, cataract, and other ocular surgery."
+    },
+    {
+        id:7,
+        title:'Cataract treatement', 
+        service: 'Cataract diagnosis and treatement',
+        description : "If you're ready for an alternative to glasses and/or contacts look to us for co-management of LASIK, cataract, and other ocular surgery."
+    },
+    {
+        id:8,
+        title:'Laser Eye surgery', 
+        service: 'Laser Eye and Lens surgery',
+        description : ' Our eye care center offers a full range of eye health services to clients of all ages, from children to older adults'
+    },
+    {
+        id:9,
+        title:'Pediatric Ortho', 
+        service: 'pediatric optometric',
+        description : 'Our eye care center offers a full range of eye health services to clients of all ages, from children to older adults'
+    },
+    {
+        id:10,
+        title:'Cataract treatement', 
+        service: 'Cataract diagnosis and treatement',
+        description : 'Our eye care center offers a full range of eye health services to clients of all ages, from children to older adults'
+    },
+    {
+        id:11,
         title:'Laser Eye surgery', 
         service: 'Laser Eye and Lens surgery',
         description : ' Our eye care center offers a full range of eye health services to clients of all ages, from children to older adults'
