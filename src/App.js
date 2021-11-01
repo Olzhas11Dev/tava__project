960cf827495848aba71d89ac7409229aaab5ee7b
Olzhas11Dev <olzhasdev11@gmail.com>
2021-11-01 18:20:54 +1300

    del block

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import AboutUs from './components/AboutUs'
-import BannerInfo from './components/BannerInfo'
-import ContactUs from './components/ContactUs'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import PremiumVideo from './components/PremiumVideo'
-import Service from './components/Service'
-import SocialKit from './components/SocialKit'
-import Testimonials from './components/Testimonials'
-
-
+import AboutUs from './components/AboutUs';
+import BannerInfo from './components/BannerInfo';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import PremiumVideo from './components/PremiumVideo';
+import Service from './components/Service';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <BannerInfo/>
-      <AboutUs/>
-      <Service/>
-      <Testimonials/>
-      <PremiumVideo/>
-      <SocialKit/>
-      <ContactUs/>
-      <Footer/>
+      <Navbar />
+      <BannerInfo />
+      <AboutUs />
+      <Service />
+      <Testimonials />
+      <PremiumVideo />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
